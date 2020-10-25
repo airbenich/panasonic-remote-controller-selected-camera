d5035b36a0b37265522ee04a544b2379efe1ae6d
airbenich <vivian@erbenich.eu>
2020-10-25 18:40:59 +0100

    adds websocket server with current selected camera broadcast

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
-console.log('Panasonic AW-RP150 Selectedt Camera AUX Switcher');
+console.log('\033c'); // clear terminal
+console.log('Panasonic AW-RP150 Selected Camera AUX Switcher');
 
 // Include Nodejs' net module.
 const Net = require('net');
@@ -12,7 +13,7 @@ const server = new Net.Server();
 // The server listens to a socket for a client to make a connection request.
 // Think of a socket as an end point.
 server.listen(port, function() {
-    console.log('Server listening for connection requests on socket localhost: ' + port);
+    console.log('Server listening for panasonic AW-RP150 connection requests on socket localhost: ' + port);
 });
 
 // When a client requests a connection with the server, the server creates a new
@@ -60,6 +61,7 @@ cameraButtonMapping[4] = 5;
 
 function selectCamera(number) {
     console.log('Selected Camera ' + number);
+    sendCommandToAllWebsocketClients('selectedCamera', number);
     sendRequestToStreamDeck(10,cameraButtonMapping[number]);
 }
 
@@ -80,4 +82,53 @@ function sendRequestToStreamDeck(bank,button) {
     }).on("error", (err) => {
       console.log("Error: " + err.message);
     });
+}
+
+
+
+
+
+
+
+
+
+/*
+ * Websocket server
+ *
+ * 
+ */
+var websocketClients = new Array();
+
+var io = require('socket.io')(http);
+// Authentication
+io.use(function(socket, next){
+  // console.log("Query: ", socket.handshake.query);
+  // return the result of next() to accept the connection.
+  if (socket.handshake.query.authentication == "sDJZn16TuP7zu82a") {
+      return next();
+  }
+  // call next() with an Error if you need to reject the connection.
+  next(new Error('Authentication error'));
+});
+
+// on conncection
+io.on('connection', function(socket){
+  console.log('Websocket client connected');
+
+  // add client to clientlist
+  websocketClients.push(socket);
+
+  // return 'connected'
+  socket.emit('connetion',true);
+
+
+  socket.on('disconnect', function(){
+    console.log('Client disconnected');
+  });
+});
+
+function sendCommandToAllWebsocketClients(key, value) {
+  websocketClients.forEach(function (websocketClient) {
+    websocketClient.emit(key, value);
+  });
 }
